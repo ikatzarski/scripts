62e19d620e8af6e7ebea4c018ae4d68a9e939ee7
Ivan Katzarski <10603133+ikatzarski@users.noreply.github.com>
2023-02-08 20:21:48 +0000

    Remove the Ansible playground files

--- ansible-playground/terraform/ansible/templates/hello.ts
+++
@@ -1,9 +0,0 @@
-import { serve } from "https://deno.land/std@0.57.0/http/server.ts";
-
-const s = serve({ port: 80 });
-
-console.log("http://localhost:80/");
-
-for await (const req of s) {
-  req.respond({ body: "Hello World\n" })
-}
